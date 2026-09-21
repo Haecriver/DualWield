397a3d27d86a4673dd2f51dd4f47d1d8f1de7c20
Haecriver <haecriver@outlook.fr>
2026-09-22 00:17:15 +0200

    Add a new project

---
+++ Source/CECompat/CECompat.cs
@@ -0,0 +1,12 @@
+﻿using Verse;
+
+namespace DualWield
+{
+    public class VGECompat : Mod
+    {
+        public VGECompat(ModContentPack content) : base(content)
+        {
+            Log.Message("CECompat is loaded !");
+        }
+    }
+}
