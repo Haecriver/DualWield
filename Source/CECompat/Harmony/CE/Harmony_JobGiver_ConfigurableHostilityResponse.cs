830eea5cc0ef506ee1090d7046ad41554da2973f
Haecriver <haecriver@outlook.fr>
2026-09-26 17:13:42 +0200

    Patch JobGiver_ConfigurableHostilityResponse

---
+++ Source/CECompat/Harmony/CE/Harmony_JobGiver_ConfigurableHostilityResponse.cs
@@ -0,0 +1,36 @@
+﻿using CombatExtended;
+using CombatExtended.AI;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+using Verse.AI;
+
+namespace DualWield.CECompat.Harmony
+{
+    [HarmonyPatch(typeof(JobGiver_ConfigurableHostilityResponse), "TryGetAttackNearbyEnemyJob")]
+    internal class Harmony_JobGiver_ConfigurableHostilityResponse
+    {
+        internal static void Postfix(Pawn pawn, ref Job __result)
+        {
+            if (__result != null && __result.def == JobDefOf.AttackStatic)
+            {
+                if (!pawn.equipment.Primary.IsOffHand() && pawn.equipment.TryGetOffHandEquipment(out ThingWithComps offHandEquip))
+                {
+                    // Check for reload
+                    var ammoComp = offHandEquip.TryGetComp<CompAmmoUser>();
+                    if (ammoComp == null)
+                    {
+                        return;
+                    }
+
+                    if (!ammoComp.CanBeFiredNow)
+                    {
+                        __result = ammoComp.HasAmmo && !(pawn.jobs?.curDriver is IJobDriver_Tactical) ?
+                                   JobMaker.MakeJob(CE_JobDefOf.ReloadWeapon, pawn, offHandEquip) :
+                                   JobMaker.MakeJob(JobDefOf.AttackMelee, __result.targetA);
+                    }
+                }
+            }
+        }
+    }
+}
