2e419b9eef470500c91cd1506a9349ff1e3b1e73
Haecriver <haecriver@outlook.fr>
2026-09-26 19:31:12 +0200

    Fix shield when equip from floor

--- Source/CECompat/Harmony/DualWield/Ext_ThingDef.cs
+++ Source/CECompat/Harmony/DualWield/Ext_ThingDef.cs
@@ -1,5 +1,4 @@
 ﻿using HarmonyLib;
-using RimWorld.BaseGen;
 using Verse;
 
 namespace DualWield.CECompat.Harmony
---
+++ Source/CECompat/Harmony/DualWield/FloatMenuOptionProvider_Equip.cs
@@ -0,0 +1,19 @@
+﻿using CombatExtended;
+using HarmonyLib;
+using RimWorld;
+using Verse;
+
+namespace DualWield.CECompat.Harmony
+{
+    [HarmonyPatch(typeof(FloatMenuOptionProvider_Equip), "GetSingleOptionFor")]
+    public static class FloatMenuOptionProvider_Equip_GetSingleOptionFor
+    {
+        public static void Postfix(Thing clickedThing, FloatMenuContext context, ref FloatMenuOption __result)
+        {
+            if (CE_Utility.HasShield(context.FirstSelectedPawn))
+            {
+                __result = new FloatMenuOption("CannotEquip".Translate(clickedThing.LabelShort) + " " + "DW_AsOffHand".Translate() + " (" + "DW_OffHandWithShield".Translate() + ")", null);
+            }
+        }
+    }
+}
